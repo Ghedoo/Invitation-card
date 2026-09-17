76fe23866d5aa3dc7af004372de29c6b6601f704
Ghedoo <raghedalkadre997@gmail.com>
2026-09-17 17:04:12 +0300

    poto1

--- src/sections/OurStory/OurStory.jsx
+++ src/sections/OurStory/OurStory.jsx
@@ -1,7 +1,7 @@
 import RevealOnScroll from "../../components/RevealOnScroll/RevealOnScroll";
 import OrnamentDivider from "../../components/OrnamentDivider/OrnamentDivider";
 import { story } from "../../data/content";
-import storyImg from "../../assets/images/Gemini_Generated_Image_sbphs3sbphs3sbph.jpeg";
+import storyImg from "../../assets/images/ragheed&bassant.jpeg";
 import "./OurStory.css";
 
 export default function OurStory() {
